486a1a18f48d9ba736baf706724fa081dffb497e
jinsung <tjd610@naver.com>
2021-08-12 14:39:07 +0900

    code

--- src/com/care/def/MainClass.java
+++ src/com/care/def/MainClass.java
@@ -3,7 +3,6 @@ package com.care.def;
 
 import java.util.Scanner;
 
-import com.care.tv_jinsung.Tv_jinsung;
 import com.care.tv_ladieslog.Tv_ladieslog;
 
 
@@ -11,7 +10,6 @@ public class MainClass {
 	public static void main(String[] args) {
 
 		Scanner input =new Scanner(System.in);
-		Tv_jinsung tv_jinsung = new Tv_jinsung();
 		Tv_ladieslog tv_ladieslog =new Tv_ladieslog();
 
 
@@ -35,7 +33,6 @@ public class MainClass {
 			num=input.nextInt();
 			switch(num) {
 			case 1:
-				tv_jinsung.display();
 				tv_ladieslog.display();
 
 				System.out.println("≥√¿Â∞Ì"); 
