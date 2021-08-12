35f55b6d4b90d30d5f65e5095e6b50fd1f43e3d2
jinsung <tjd610@naver.com>
2021-08-12 11:43:26 +0900

    tv기능

--- src/com/care/def/MainClass.java
+++ src/com/care/def/MainClass.java
@@ -1,5 +1,16 @@
 package com.care.def;
 
-public class MainClass {
+import java.util.Scanner;
 
+import com.care.tv_jinsung.Tv_jinsung;
+
+public class MainClass {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		Tv_jinsung tv = new Tv_jinsung();
+		
+		tv.display();
+		
+		
+	}
 }
