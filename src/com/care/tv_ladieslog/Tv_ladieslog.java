3ff518f5710fbd2c223a100bd8f76c35177ced4c
matia005 <matia0529@gmail.com>
2021-08-12 11:23:22 +0900

    내코드 업로드

--- src/com/care/tv_ladieslog/Tv_ladieslog.java
+++ src/com/care/tv_ladieslog/Tv_ladieslog.java
@@ -1,5 +1,21 @@
 package com.care.tv_ladieslog;
 
-public class Tv_ladieslog {
+import java.util.Scanner;
 
+public class Tv_ladieslog {
+	public void display() {
+		System.out.println("냉장고 하는 기능입니다. ");
+		Scanner input =new Scanner(System.in);
+		int num;
+		System.out.println("1. up 2. down");
+		num=input.nextInt();
+		if(num==1) tempUp();
+		else tempDown();
+	}
+	public void tempUp() {
+		System.out.println("온도를 올립니다. ");
+	}
+	public void tempDown() {
+		System.out.println("온도를 낮춥니다. ");
+	}
 }
